2b3fd475e0c31165469bdfc1d1dd99597802a1c3
0912826223 <41816268+0912826223@users.noreply.github.com>
2018-07-31 14:26:37 +0800

    聖誕樹

---
+++ 聖誕樹Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class P
+{
+    {
+        Console.WriteLine("");
+
+        int star = 1;
+        int line = 1;
+
+        int space;
+        space = 4;
+
+        int b = 1;
+        while(b<=5)
+
+
+        {
+            int i = 1;
+            while (i <= space)
+            {
+                Console.Write(" ");
+              i+=1;
+            }
+
+            {
+                int t = 1;
+                while (t <= star)
+                {
+                    Console.Write("*");
+                   t+= 1;
+                }
+            }
+
+            {
+                int p = 1;
+                while (p <= line)
+                {
+                    Console.Write("\n");
+                 p += 1;
+                }
+
+            }
+
+           space -= 1;
+           star += 2;
+            b += 1;
+
+
+        }
+
+
+        Console.WriteLine("結束");
+        Console.Write("\n");
+    }
+}
